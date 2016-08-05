fbd4b3b1e4ae36f5dc96b6cb7c0469b2436e8cb8
wdeqin <wdeqin@192.168.133.129>
2016-08-06 01:10:56 +0800

    add log4j

--- src/main/java/com/github/wdeqin/flyit/web/expriment/HelloWorldController.java
+++ src/main/java/com/github/wdeqin/flyit/web/expriment/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.github.wdeqin.flyit.web.expriment;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +10,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
+	
+	private static final Logger logger = LogManager.getLogger("com.flyit.web");
 
 	@RequestMapping("/hello")
 	public String hello(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
 			Model model) {
 		model.addAttribute("name", name);
-		// returns the view name
+
+		logger.trace("hello: " + name);
+		
 		return "helloworld";
 
 	}
