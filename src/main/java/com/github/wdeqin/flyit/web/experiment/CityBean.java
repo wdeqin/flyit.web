fd19833368de476de1f13d0d87cea94b8fec0859
wdeqin <wdeqin@192.168.133.129>
2016-08-07 11:09:14 +0800

    delegate transaction to spring failed

--- src/main/java/com/github/wdeqin/flyit/web/experiment/CityBean.java
+++ src/main/java/com/github/wdeqin/flyit/web/experiment/CityBean.java
@@ -2,32 +2,35 @@ package com.github.wdeqin.flyit.web.experiment;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.wdeqin.flyit.data.dao.CityMapper;
 import com.github.wdeqin.flyit.data.model.City;
 
-public class CityBean implements RmiHandler{
+public class CityBean implements RmiHandler {
+	private Logger logger;
 	private CityMapper cityMapper;
-	private Logger logger ;
-	
+
 	public CityBean() {
 		logger = LogManager.getLogger("com.flyit.web");
 	}
-	
+
 	public void setCityMapper(CityMapper cityMapper) {
 		this.cityMapper = cityMapper;
 	}
-	
+
+	@Transactional("transactionManager")
 	public String handle(RmiPack pack) {
-		logger.debug(pack);
-		Integer id = (Integer)pack.getInput().get("id");
-		City c = cityMapper.selectByPrimaryKey(id);
-		cityMapper.updateByPrimaryKeySelective(c);
-		cityMapper.updateByPrimaryKeySelective(c);
-		ObjectMapper om = new ObjectMapper();
+
 		try {
+			logger.debug(pack);
+			Integer id = (Integer) pack.getInput().get("id");
+			City c = cityMapper.selectByPrimaryKey(id);
+			cityMapper.updateByPrimaryKeySelective(c);
+			cityMapper.updateByPrimaryKeySelective(c);
+			ObjectMapper om = new ObjectMapper();
 			return om.writeValueAsString(c);
 		} catch (JsonProcessingException e) {
 			logger.error(e);
